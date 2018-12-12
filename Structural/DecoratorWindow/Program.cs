3bf400919379b830a40139cb0bdf90eecfa68815
yura <yura_k@pc-service.dp.ua>
2018-12-12 10:38:46 +0200

    Structural  DecoratorWindow scroll

---
+++ Structural/DecoratorWindow/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Назначение: 
+//	динамически добавляет объекту новые обязанности. 
+//	Является гибкой альтернативой порождению подклассов с целью расширения функциональности.
+
+namespace Decorator {
+
+	class Program {
+		
+		static void Main(string[] args) {
+			IWindow window = new HorizontalScrollbarWindow(new VerticalScrollbarWindow(new SimpleWindow()));
+			Console.WriteLine(window.GetDescription());
+
+			Console.ReadKey();
+		}
+	}
+}
